aa4fd65ca8853573edfaf9645321088596bb2fd6
parasmeena96 <parasmeena96@gmail.com>
2019-08-22 23:56:40 +0530

    callBack function

--- js.js
+++ js.js
@@ -50,5 +50,69 @@ function function_Name(a,b){
 }
 document.write( function_Name (5,5) );
 
-// function declaration in Arrow function-------------------------------------------------------
+// function declaration in Arrow function-------------------------------------
 (a,b) => a+b;
+//Arrow function---------------------------------------------------------------------------------------------------
+
+
+
+
+// callback function ---------------------------------------------------------------------------------------------
+ function show(){
+     document.write("<br> <br>  hello i am callback function");
+ }
+function callback_function(callback){
+callback();
+}
+callback_function(show);
+
+// with argument -------------------------------
+function show_argument (a){
+    document.write("<br><br>i am with argument " + a);
+}
+function callback_function_argument(callback_argument){
+var a = "<strong>hello</strong>";
+    callback_argument(a);
+}
+callback_function_argument(show_argument);
+
+// Different type callback function
+
+function callback_function(callback){
+    callback();
+    }
+    callback_function(function(){
+        document.write("<br> <br>  <strong>hello i am callback function</strong>");
+    });
+
+// callback in arrow function
+
+function callback_function_arrow(callback){
+    var b = 200;
+    callback(b);
+    }
+    callback_function_arrow(b =>document.write("<br> <br>  <strong> hello i am callback function with arrow function </strong>" + b +"%"));
+
+
+
+//callback synchronous function
+
+function show(){
+    document.write("<br> <br>  hello i am callback synchronous function ");
+}
+function callback_function(callback){
+callback();
+}
+callback_function(show);
+document.write(" End");
+
+
+//callback asynchronous function 
+setTimeout(function synchronous_function(){
+document.write("i am synchronous function");
+},
+10000);
+
+document.write(" END ");
+
+// callback function ----------------------------------------------------------------------------------------------------
